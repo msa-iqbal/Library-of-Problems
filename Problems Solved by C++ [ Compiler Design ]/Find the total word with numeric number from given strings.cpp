487745d258fd018a6a91648655578e3dcd0c7e52
Muhammad S.A. Iqbal <44881940+msa-iqbal@users.noreply.github.com>
2024-08-19 03:48:40 +0600

    Add files via upload

---
+++ Problems Solved by C++ [ Compiler Design ]/Find the total word with numeric number from given strings.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+int main(){
+
+    char string[100];
+    int i=0, count=0, strlng=0, result=0;
+    int num, num_count=0, sum=0;
+
+    printf("\n \n Enter the String: ");
+    scanf("%[^\n]string", string);
+
+    /// Word Count Without Numeric Numbers
+    for(i=0; string[i] !='\0'; i++){
+    if(string[i] == ' ')
+    count++;
+    if(isdigit(string[i]))
+    strlng++;
+    }
+    result=count-strlng;
+
+    printf(" Total number of words are: %d \n", count+1);
+    printf(" Total number of words are (Without Digits): %d \n", result+1);
+    printf(" Total number of digits: %d \n", strlng);
+    return 0;
+}
+
+/* ==== Output: ====
+Enter the String: I am 9 year old
+Total number of words are: 5
+Total number of words are (Without Digits): 4
+Total number of digits: 1
+*/
---
+++ Problems Solved by C++ [ Compiler Design ]/Remove duplicate words from given strings.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char a[100], b[20][20];
+    int i, j = 0, k = 0, n, m;
+    printf(" Enter the String: ");
+    scanf("%[^\n]s", a);
+    for (i = 0; a[i] != '\0'; i++){
+        if (a[i] == ' '){
+            b[k][j] = '\0';
+            k++;
+            j = 0;
+        }else{
+            b[k][j] = a[i];
+            j++;
+        }
+    }
+    b[k][j] = '\0';
+    for (i = 0;i <= k;i++){
+        for (j = i + 1;j <= k;j++){
+            if (strcmp(b[i], b[j]) == 0){
+                for (m = j;m <= k;m++){
+                    strcpy(b[m], b[m + 1]);
+                    k--;
+                }
+            }
+        }
+    }
+    for (n = 0;n <= k;n++){
+        printf(" %s\n", b[n]);
+    }
+    return 0;
+}
